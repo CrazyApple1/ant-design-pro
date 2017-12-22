efe1ae2871a1f5557f185c1a636981ef64cfa083
PKAQ <pkaq@msn.com>
2017-12-22 10:01:40 +0800

    module mock data

---
+++ mock/module.js
@@ -0,0 +1,67 @@
+import { getUrlParams } from './utils';
+// 树形数据
+let data = [{
+  key: 1,
+  name: 'John Brown sr.',
+  age: 60,
+  address: 'New York No. 1 Lake Park',
+  children: [{
+    key: 11,
+    name: 'John Brown',
+    age: 42,
+    address: 'New York No. 2 Lake Park',
+  }, {
+    key: 12,
+    name: 'John Brown jr.',
+    age: 30,
+    address: 'New York No. 3 Lake Park',
+    children: [{
+      key: 121,
+      name: 'Jimmy Brown',
+      age: 16,
+      address: 'New York No. 3 Lake Park',
+    }],
+  }, {
+    key: 13,
+    name: 'Jim Green sr.',
+    age: 72,
+    address: 'London No. 1 Lake Park',
+    children: [{
+      key: 131,
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 2 Lake Park',
+      children: [{
+        key: 1311,
+        name: 'Jim Green jr.',
+        age: 25,
+        address: 'London No. 3 Lake Park',
+      }],
+    }],
+  }],
+}, {
+  key: 2,
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}];
+// 获取模块数据
+export function getModule(req, res, u) {
+  let url = u;
+  if (!url || Object.prototype.toString.call(url) !== '[object String]') {
+    url = req.url; // eslint-disable-line
+  }
+  const params = getUrlParams(url);
+
+  let dataSource = [...data];
+
+  if (res && res.json) {
+    res.json(dataSource);
+  } else {
+    return dataSource;
+  }
+}
+
+export default {
+  getModule
+}
