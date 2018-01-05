c57737fe2a9060cf36710e4e1e7339aebc1b7904
PKAQ <pkaq@msn.com>
2018-01-05 14:00:34 +0800

    dict新增mock

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -9,7 +9,7 @@ import { getNotices } from './mock/notices';
 import {  delay } from 'roadhog-api-doc';
 import { getModule } from './mock/module';
 import { getOrg } from './mock/orginization';
-import { listDict,getDict, deleteDictItem } from './mock/dict';
+import { listDict,getDict, deleteDictItem,addDictItem } from './mock/dict';
 
 // 是否禁用代理
 const noProxy = process.env.NO_PROXY === 'true';
@@ -65,6 +65,7 @@ const proxy = {
   'GET /dict/getDict': getDict,
   'GET /dict/listDict': listDict,
   'POST /dict/deleteDictItem': deleteDictItem,
+  'POST /dict/addDictItem': addDictItem,
   'GET /orginization/list': getOrg,
   'GET /goods/list': getGoods,
   'POST /goods/list': {
