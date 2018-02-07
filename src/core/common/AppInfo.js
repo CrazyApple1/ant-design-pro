d2ba71a86b0754d0b6b2bf1594c2cec586c2d175
PKAQ <pkaq@msn.com>
2018-02-07 11:56:29 +0800

    init commit

--- src/core/common/AppInfo.js
+++
@@ -1,3 +0,0 @@
-export const title = 'PKAQ Design';
-export const copyRight = ' 2018 PKAQ 体验技术部出品 ';
-export const description = 'power by Ant Design';
--- src/core/service/global.js
+++
@@ -1,5 +0,0 @@
-import request from '../utils/request';
-// 加载组织列表
-export async function getUserMenu(params) {
-  return request(`/module/getUserMenu`);
-}
