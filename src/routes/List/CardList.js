48fb71eac41eb6b0a557bb82ce477b544099cdf2
PKAQ <pkaq@msn.com>
2018-02-17 09:09:03 +0800

    bug fix

--- src/routes/List/CardList.js
+++ src/routes/List/CardList.js
@@ -67,7 +67,11 @@ export default class CardList extends PureComponent {
                 <Card hoverable className={styles.card} actions={[<a>操作一</a>, <a>操作二</a>]}>
                   <Card.Meta
                     avatar={<img alt="" className={styles.cardAvatar} src={item.avatar} />}
-                    title={<a href="#">{item.title}</a>}
+                    title={<a href="#">
+                        <Ellipsis length={16}>
+                          {item.title}
+                        </Ellipsis>
+                    </a>}
                     description={(
                       <Ellipsis className={styles.item} lines={3}>{item.description}</Ellipsis>
                     )}
